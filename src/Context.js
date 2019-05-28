7bc530756d038693e228be8d9e3a7eaf380e8dcc
Cristian Lucero <cristianjlucero32@gmail.com>
2019-05-28 15:18:13 -0700

    Created FeaturedRooms comp with loading spinner

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import items from './data';
 
 // Creates a context object.
 // When React renders a comp that subscribes to this Context object,
@@ -9,9 +10,35 @@ const RoomContext = React.createContext();
 
 export default class RoomProvider extends Component {
   state = {
-    greeting: 'hello',
-    name: 'jon'
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
   };
+
+  //   Grabs list of rooms on comp mount
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    // If rooms are featured, show them.
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
 
   render() {
     return (
