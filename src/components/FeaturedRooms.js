a694c2ff318429590113ef5e5440a737d7cec4ac
Cristian Lucero <cristianjlucero32@gmail.com>
2019-05-28 15:26:29 -0700

    Set up component to shoe either loading spinner or featured rooms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import { RoomContext } from '../Context';
 import Loading from './Loading';
 import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRooms extends Component {
   // Cannot grab context in a functional component, only class comp
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
     return (
-      <div>
-        <Room />
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {/* If loading is true, shoe spinner, if not shoe rooms */}
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
